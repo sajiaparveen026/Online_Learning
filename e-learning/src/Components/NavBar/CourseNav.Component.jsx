1eff87e229e370f8f4cd8f7fba405e863ceb1795
sajiaparveen026 <sajia026@gmail.com>
2023-04-09 22:12:44 +0530

    NavBar and Banner Section

--- e-learning/src/Components/NavBar/CourseNav.Component.jsx
+++ e-learning/src/Components/NavBar/CourseNav.Component.jsx
@@ -1,25 +1,84 @@
-import React from 'react'
-import { BiShareAlt } from 'react-icons/bi'
+import React, { useState } from 'react'
+import { BiMenu } from 'react-icons/bi'
+import { Link } from 'react-router-dom';
+import { FiBookmark } from "react-icons/fi";
 
 
 const Navsm = () =>{
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
     return <>
       <div className='text-gray-700 flex items-center justify-between'>
         <div className='text-bold text-3xl'>
             <h1 className='text-white'>E-Learning</h1>
         </div>
-        <div className='w-8 h-8'>
-      <BiShareAlt className='w-full h-full text-white' />
+       <div className="relative">
+      <button
+        className="text-white font-bold text-2xl py-4 pl-4 pr-6"
+        onClick={toggleDropdown}
+      >
+        <BiMenu className='text-3xl'/>
+      </button>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 bg-white rounded-md shadow-lg">
+          {/* Dropdown content */}
+           <ul className="py-2 w-80 bg-gray-300 rounded-md ">
+            <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 items-center cursor-pointer">
+             <Link to="/"className='text-xl' >Home</Link>
+             <hr />
+            </li>
+            <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
+               <button className="text-white bg-red-400 w-30  h-8 text-2xl rounded-md">
+          <span className="text-white  flex items-center  ml-3 mr-2">
+           About
+          </span>
+    </button>
+      
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
 
         </div>
     </>
 }
+
+const NavLg = () =>{
+  return <>
+     <div className="container flex mx-auto px-4 items-center justify-between">
+  <div className="flex items-center w-1/2">
+    <div className="w-50 h-50">
+      <h1 className="text-white font-bold text-3xl">E-Learning</h1>
+    </div>
+    <div className="w-10 h-10 flex items-center">
+      <Link to="/" className="text-white font-semibold text-2xl py-4">
+        Home
+      </Link>
+    </div>
+  </div>
+    <div className='flex ml-auto'>
+        <button className="text-white bg-red-400 w-40  h-10 text-2xl rounded-md">
+          <span className="text-white font-bold flex items-center gap-2 ml-5 hover:text-white">
+           <FiBookmark />  About
+          </span>
+    </button>
+    </div>
+</div>
+  </>
+}
+// Main
 const CourseNav = () => {
   return <>
    <nav className='bg-darkBackground-700 border-b-2 lg:border-b-0  p-4'>
       <div className='md:hidden'>
         <Navsm />
+      </div>
+       <div className='hidden w-full lg:flex md:flex'>
+        <NavLg />
       </div>
      </nav>
   </>
--- e-learning/src/Pages/Home.Page.jsx
+++ e-learning/src/Pages/Home.Page.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import DefaultLayoutHoc from '../layouts/Default.Layout'
+import React from 'react';
+import DefaultLayoutHoc from '../layouts/Default.Layout';
+import Image from '../Components/Images/pngtree-cartoon-flat-girl-character-design-free-download-image_1317099.jpg';
+import Image1 from '../Components/Images/12.png'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      
+      <div className="flex justify-center items-center bg-gray-100 ">
+        <img src={Image} alt="girl" className='my-0 w-80 h-60 opacity-80 transition-opacity duration-1000 ease-in-out hover:opacity-100" hidden lg:block' />
+      </div>
+      <div className=" flex justify-center items-center bg-gray-100 ">
+        <img src={Image1} alt="girl" className='w-1/2 h-auto hidden lg:block'/>
+      </div>
+     
 
-export default DefaultLayoutHoc(HomePage)
+      <div className=''>
+      <div className=" flex justify-center items-center bg-gray-100 ">
+        <img src={Image} alt="girl" className='my-0 w-60 h-60 hidden md:block' />
+      </div>
+      <div className=" flex justify-center items-center bg-gray-100 ">
+        <img src={Image1} alt="girl" className='w-90 h-50 hidden md:block'/>
+      </div>
+      </div>
+
+      <div className='md:hidden'>
+      <div className=" flex justify-center items-center bg-gray-100 ">
+        <img src={Image} alt="girl" className='my-0 w-60 h-60 ' />
+      </div>
+      <div className=" flex justify-center items-center bg-gray-100 ">
+        <img src={Image1} alt="girl" className='w-100 h-50 '/>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default DefaultLayoutHoc(HomePage);
